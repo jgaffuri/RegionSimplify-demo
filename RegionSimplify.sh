a7f07f7e950f4748193de3c872be627281056310
jgaffuri <jgaf@proton.me>
2023-02-07 15:09:53 +0100

    a

--- RegionSimplify.sh
+++ RegionSimplify.sh
@@ -2,7 +2,7 @@
 
 mkdir -p test_out
 
-for dataset in "philippines" #"chile_" "bangladesh_" "china_mainland_" "panama_" "indonesia_"
+for dataset in "chile" "bangladesh" "panama" "indonesia" "philippines"
 do
 	for scaleM in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "30" "40" "50" "60" "100" "200" "300" "400" "500"
 	do
