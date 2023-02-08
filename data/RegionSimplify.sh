197d45eb02eb1b9da515327fd9e6432a4db9fd0f
jgaffuri <jgaf@proton.me>
2023-02-08 16:19:53 +0100

    a

--- data/RegionSimplify.sh
+++ data/RegionSimplify.sh
@@ -4,7 +4,7 @@ mkdir -p out
 
 for dataset in "france" #"europe" #"chile" "bangladesh" "panama" "indonesia" "philippines"
 do
-	for scaleM in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "30" "40" "50" "60" "100" "200" "300" "400" "500"
+	for scaleM in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "30" "40" "50" "60"
 	do
     	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
 		java -jar ../RegionSimplify.jar -i "in/"$dataset".geojson" -o "out/"$dataset"-"$scaleM"M.geojson" -s $scaleM"000000" -inb 2
