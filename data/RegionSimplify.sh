a562106106121172fef148ae946bb4bc40c347a0
jgaffuri <jgaf@proton.me>
2023-02-08 20:26:38 +0100

    a

--- data/RegionSimplify.sh
+++ data/RegionSimplify.sh
@@ -4,7 +4,7 @@
 
 for dataset in "bangladesh" "indonesia" "philippines" "chile" "panama" "france" "europe"
 do
-	for scaleM in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "30" "40" "50" "60"
+	for scaleM in "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "30" "40" "50"
 	do
     	echo "Generalisation for "$dataset" - 1:"$scaleM"M"
 		java -jar ../RegionSimplify.jar -i "in/"$dataset".geojson" -o "out5/"$dataset"-"$scaleM"M.geojson" -s $scaleM"000000" -inb 5
